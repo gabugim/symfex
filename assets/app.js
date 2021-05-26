838dbbb485d4fce8ffe1b68b16dd342900340a48
Gaetan <gabu-g@hotmail.fr>
2021-05-26 21:29:04 +0200

    c two bo sass

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+require('bootstrap');
+console.log('Hello Webpack Encore !')
