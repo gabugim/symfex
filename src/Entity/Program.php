d25bcfc64e523e82bc26e51cc8c456353954b454
Gaetan <gabu-g@hotmail.fr>
2021-06-01 15:47:53 +0200

    ajout

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -35,7 +35,7 @@ class Program
     private $poster;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Category::class)
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="programs")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
